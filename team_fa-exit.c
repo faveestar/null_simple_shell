d66e321e732731ef3466fd59fca0200eb60066f2
Azeez Saka <saoconcept1@gmail.com>
2023-08-17 03:10:08 -0700

    add team_fa-exit.c file

---
+++ team_fa-exit.c
@@ -0,0 +1,14 @@
+#include "team_fa_lib.h"
+
+/**
+* process_exit_command -This function help exit process.
+*
+* @void: void parameter
+* Return: Always void (success).
+*/
+void process_exit_command(void)
+{
+_printf("Proccess terminate successful\n");
+exit(EXIT_SUCCESS);
+}
+
